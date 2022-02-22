c952840e8ba04b958308c4aaa2425d68e0cddd9b
DobbsZerker <Nickdobbin@hotmail.com>
2022-02-22 11:00:47 -0330

    updated some bugs

--- config.js
+++ config.js
@@ -7,22 +7,31 @@ function configApp() {
     const myArgs = process.argv.slice(2);
         switch (myArgs[1]) {
             case '--show':
-                if(DEBUG) console.log(myArgs[0], ' - display the configuration file');
-                let viewConfig = fs.readFileSync('config.json');
-                let viewConfigString = JSON.parse(viewConfig)
-                console.log(viewConfigString)
+                showConfig();
+                if(DEBUG) console.log('display the configuration file');
                 break;
             case '--set':
                 setConfig();
                 if(DEBUG) console.log('configApp.setConfig() --set');
                 break;
             case '--reset':
-            createFolder()
+            resetConfig();
             if(DEBUG) console.log('configApp.resetConfig() --reset');
             default:
                 if(DEBUG) console.log('Config - default');
         }
     }
+
+    function showConfig() {
+        if(DEBUG) console.log('display the configuration file');
+                let viewConfig = fs.readFileSync('config.json');
+                let viewConfigString = JSON.parse(viewConfig);
+                console.log(viewConfigString);
+    }
+
+    function setConfig() {
+
+    } 
 
 
 module.exports ={ 
